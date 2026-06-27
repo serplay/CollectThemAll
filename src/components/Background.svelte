e3d8e9dabd282d0c4aef60b1cfab5a1cae0a9632
Infinity <kuba20160101@gmail.com>
2026-06-27 18:58:43 +0200

    feat: Added Background

---
+++ src/components/Background.svelte
@@ -0,0 +1,16 @@
+<div class="grid-bg"></div>
+
+<style>
+  .grid-bg {
+    position: fixed;
+    inset: 0;
+    z-index: -1;
+    background-color: #010201;
+    background-image: linear-gradient(to right, #0f0f10 1px, transparent 1px),
+      linear-gradient(to bottom, #0f0f10 1px, transparent 1px);
+    background-size: 1rem 1rem;
+    background-position: center center;
+    filter: blur(1px);
+    pointer-events: none;
+  }
+</style>
